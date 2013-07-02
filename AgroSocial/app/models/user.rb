0a44dfe0ec9a0effc7ac64c93ad682abad0d1d50
72MiguelGomes <jmbg.41@gmail.com>
2013-07-02 11:06:11 +0100

    sha1 function added in User model

--- AgroSocial/app/models/user.rb
+++ AgroSocial/app/models/user.rb
@@ -1,3 +1,9 @@
+require 'digest/sha1'
+
 class User < ActiveRecord::Base
   	attr_accessible :adress, :age, :email, :genre, :name, :password
+
+	def self.encrypt(pass)
+		Digest::SHA1.hexdigest(pass)
+	end
 end
