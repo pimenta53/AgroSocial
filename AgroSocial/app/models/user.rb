53cc1087ced5f454a43ccc1d648788e9f4a57b8f
72MiguelGomes <jmbg.41@gmail.com>
2013-07-02 11:13:48 +0100

    authenticate method added

--- AgroSocial/app/models/user.rb
+++ AgroSocial/app/models/user.rb
@@ -3,6 +3,13 @@ require 'digest/sha1'
 class User < ActiveRecord::Base
   	attr_accessible :adress, :age, :email, :genre, :name, :password
 
+	def self.authenticate(name, pass)
+		user = find(:first, :conditions=>["name = ?", name])
+		return nil if user.nil?
+		return user if User.encrypt(pass)==user.password
+		nil
+	end  
+
 	def self.encrypt(pass)
 		Digest::SHA1.hexdigest(pass)
 	end
