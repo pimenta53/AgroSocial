de92a92459275d0dc52e9241658526e88a49f325
Joao Gomes <joaogomes@MacBook-Pro-de-Joao.local>
2013-07-05 22:44:48 +0100

    Users total added

--- AgroSocial/db/schema.rb
+++ AgroSocial/db/schema.rb
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20130705213913) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "age"
+    t.string   "genre"
+    t.string   "address"
+    t.boolean  "is_association"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
