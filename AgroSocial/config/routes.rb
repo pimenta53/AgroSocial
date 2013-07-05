e35d262a064982194195a951ea1ab8628e4a18e1
pimenta53 <53.andre.pimenta@gmail.com>
2013-07-05 23:42:44 +0100

    home added

--- AgroSocial/config/routes.rb
+++ AgroSocial/config/routes.rb
@@ -10,7 +10,7 @@ AgroSocial::Application.routes.draw do
 
   devise_for :users
 
-  root :to => "feeds#index"
+  root :to => "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
