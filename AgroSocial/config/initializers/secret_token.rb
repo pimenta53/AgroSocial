2d48ed0b5cc72dffc324ad900322643f6195a2cd
Joao Gomes <joaogomes@new-host-3.home>
2013-07-05 14:53:15 +0100

    project in rails 4.0 and ruby 2.0 added

---
+++ AgroSocial/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AgroSocial::Application.config.secret_key_base = 'fe278b34bd24dcc930d9c70996925b17eb6af821d864cb86f0cf6449c205268e41a4e25eb67ee5ac49ef81348f9ff90b4810dda3ea381c9f2c6c21a2dd5c477f'
